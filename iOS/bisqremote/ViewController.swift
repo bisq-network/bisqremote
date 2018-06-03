95dac223afc8b60657b55198a240af2b476dd372
Joachim Neumann <joachim@mindo.io>
2018-06-03 11:07:27 +0200

    initial commit

---
+++ iOS/bisqremote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  n
+//
+//  Created by Joachim Neumann on 03/06/2018.
+//  Copyright © 2018 joachim Neumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
